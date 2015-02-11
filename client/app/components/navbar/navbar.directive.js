cdc81e856fc05ce3f4e6c249048ec65d7c0b4a37
Tyler Henkel <tylerhenkel@hotmail.com>
2015-02-11 01:50:44 -0700

    fix navbar

--- client/app/components/navbar/navbar.directive.js
+++ client/app/components/navbar/navbar.directive.js
@@ -3,15 +3,13 @@ angular
   .directive('navbar', navbar);
 
 function navbar() {
-  {
-    var directive = {
-      link: link,
-      templateUrl: 'client/app/components/navbar/navbar.directive.html',
-      restrict: 'EA'
-    };
-    return directive;
+  var directive = {
+    link: link,
+    templateUrl: 'client/app/components/navbar/navbar.directive.html',
+    restrict: 'EA'
+  };
+  return directive;
 
-    function link(scope, element, attrs) {
-    }
+  function link(scope, element, attrs) {
   }
 }
