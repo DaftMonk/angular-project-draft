ad75082c58769c50f2a2cf649895e8e7a5552303
Tyler Henkel <tylerhenkel@hotmail.com>
2015-02-12 00:57:18 -0700

    concat all necessary js and html files

--- Brocfile.js
+++ Brocfile.js
@@ -7,6 +7,9 @@ var upstreamMergeTrees = require('broccoli-merge-trees');
 var compileModules = require('broccoli-es6-module-transpiler');
 var AMDFormatter = require('es6-module-transpiler-amd-formatter');
 var transpiler = require('es6-module-transpiler');
+var findBowerTrees = require('broccoli-bower');
+var concat = require('broccoli-concat');
+var angularTemplates = require('broccoli-angular-templates-cache');
 
 function mergeTrees(inputTree, options) {
   var tree = upstreamMergeTrees(inputTree, options);
@@ -15,20 +18,62 @@ function mergeTrees(inputTree, options) {
 }
 
 var AngularApp = function () {
-  var jsFiles = new Funnel('client/app', {
+  this.partialFiles = new Funnel('./client/app', {
+    include: [new RegExp('.*\\.html$')],
+    exclude: [new RegExp('index\\.html$')],
+    description: 'Funnel: App JS Files'
+  });
+
+  this.templates = angularTemplates(this.partialFiles, {
+    srcDir: './',
+    destDir: './',
+    strip: '.tmp/',
+    fileName:'templates.js',
+    moduleName:'testApp'
+  });
+
+  this.appJs = new Funnel('client/app', {
     include: [new RegExp('.*\\.js$')],
     exclude: [new RegExp('.*\\.spec.js$')],
     description: 'Funnel: App JS Files'
   });
 
-  this.transpiledJS = compileModules(jsFiles, {
-    formatter: new AMDFormatter(),
-    output: 'app/'
+  this.libraries = pickFiles('client/bower_components', {
+    srcDir: '.',
+    destDir: '.',
+    files: [
+      'angular/angular.js',
+      'angular-route/angular-route.js'
+    ]
   });
+
+  this.transpiledJS = compileModules(this.appJs, {
+    output: 'app.js'
+  });
+
+  this.maps = pickFiles(this.tranpiledJS, {
+    srcDir: '.',
+    destDir: '.',
+    files: [
+      '**/*.js.map'
+    ]
+  });
+
+  this.scripts = mergeTrees([this.libraries, this.transpiledJS, this.templates]);
+
+  this.concatenatedJS = concat(this.scripts, {
+    inputFiles: [
+      'angular/angular.js',
+      'angular-route/angular-route.js',
+      '**/*.js'
+    ],
+    outputFile: '/app.js'
+  });
+
 };
 
 AngularApp.prototype.toTree = function () {
-  return mergeTrees([this.transpiledJS, 'client/app']);
+  return mergeTrees([this.concatenatedJS, 'client/app', 'client/public']);
 };
 
 var broccoliTree = new AngularApp().toTree();
